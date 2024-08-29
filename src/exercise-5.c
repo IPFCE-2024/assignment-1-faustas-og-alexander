de7c97c733a7dd0990daacd1fd848fd6f1bc8cfb
Faus0021 <202108600@post.au.dk>
2024-08-29 11:16:48 +0000

    hej

--- src/exercise-5.c
+++ src/exercise-5.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 
-int main() {
+int x;
+int y;
+int max;
 
-    // printf("Hello, World!\n");
-    // int input;
-    // scanf("%d", &input);
-    
+int main(void)
+{
+    scanf("%d",&x);
+    scanf("%d",&y);
+
+    if (x <= y);
+    max = y;
+    else
+    max = x;
+
+    printf("The maximum is %d.\n", max);
+
     return 0;
+
 }
